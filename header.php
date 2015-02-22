c6e314366a1a845dcd50239c9e2f1b150ac72030
Mihkel Lind <mihkellind@Mihkels-MBP-2.Elisa>
2015-02-22 23:14:45 +0200

    webfonts added

--- header.php
+++ header.php
@@ -14,13 +14,14 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+
+
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'duunissa' ); ?></a>
-
+	
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
